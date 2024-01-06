ce387152be23b568482089ab015e54c2b4aef088
“johnnynava” <137064281+johnnynava@users.noreply.github.com>
2024-01-06 21:20:40 +0000

    Finish Contact Component
    Most of what I need to do now is use a lot of CSS to make all of this
    pretty

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,3 +1,33 @@
-const Contact = () => {};
+const contactObj = {
+  email: "JOHNNAVA@ZOHO.COM",
+  linkedIn: "https://www.linkedin.com/in/johnnynavadev/",
+  githubLink: "https://github.com/johnnynava",
+};
+
+const Contact = () => {
+  return (
+    <div className="contact">
+      <div className="contactTop">
+        <h3>GET IN TOUCH</h3>
+        <p>{contactObj.email}</p>
+      </div>
+      <div className="contactBottom">
+        <div className="contactBottomLeft">
+          <p>Johnny Nava</p>
+          <p>Surrey, United Kingdom</p>
+        </div>
+        <div className="contactBottomMid">
+          <p>Built using</p>
+          <p>React.js and CSS</p>
+        </div>
+        <div className="contactBottomRight">
+          <p>Other Links</p>
+          <a href={contactObj.linkedIn}>LinkedIn</a>
+          <a href={contactObj.githubLink}>Github</a>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Contact;
