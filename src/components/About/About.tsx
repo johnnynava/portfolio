4568333b6b319f565fc5126f7b301629a3b3bfbb
“johnnynava” <137064281+johnnynava@users.noreply.github.com>
2024-01-31 05:42:05 +0000

    Modify bio

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -8,7 +8,7 @@ type AboutMe = {
 
 const aboutMe: AboutMe = {
   summary:
-    "Passionate and detail-oriented Front-End Developer with 5 years of experience in the tech industry as a Network Engineer. Aiming to transition to a full-time Web Developer job that will allow me to use my programming and debugging skills to tackle challenges that will provide value to customers and my employer.",
+    "Passionate, self-motivated and detail-oriented Front-End Developer with 5 years of experience in the tech industry as a Network Engineer. Aiming to transition to a role that will allow me to pursue my true passion for Software Development and tackle challenges that will provide values to customers and my employer.",
   currentSkills: [
     "React.js",
     "JavaScript",
