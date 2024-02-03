263cbf20fc22f67d526bd0977cb87bc6ec1ed64c
“johnnynava” <137064281+johnnynava@users.noreply.github.com>
2024-02-03 13:17:16 +0000

    Fix bug with canvas height
    To do this I used a useEffect for the variable of height (which is
    initialized to the root element height) and an event listener
    tracking any scroll to compare the value of height to the value of the
    root element height, if the latter changes and the user scrolls or
    resizes the window, it will trigger a re-render of the background anima-
    tion so now it doesn't break for slow connections or window re-sizing!

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,27 +1,32 @@
-import { FC, useEffect } from "react";
+import { FC, useEffect, useState } from "react";
 import * as THREE from "three";
 
 const Hero: FC = () => {
-  useEffect(()=>{
-    const scene = new THREE.Scene();
+  const [height,setHeight] = useState(document.getElementById("root")!.offsetHeight)
 
+  useEffect(()=>{
+    window.addEventListener("scroll", ()=>{
+      if(height !== document.getElementById("root")!.offsetHeight){
+        setHeight(document.getElementById("root")!.offsetHeight);
+      }
+    })
+    const scene = new THREE.Scene();
     const pov = 50;
     const camera = new THREE.PerspectiveCamera(pov, window.innerWidth / window.innerHeight);
     camera.position.z = 65;
 
     const canvas = document.getElementById("heroCanvas");
 
-    const height = document.getElementById("root")?.offsetHeight
     const renderer = new THREE.WebGLRenderer({
-      canvas: canvas!, 
-      antialias:true,
-      alpha: true
+    canvas: canvas!, 
+    antialias:true,
+    alpha: true
     })
     renderer.setClearColor( 0x000000, 0 );
     renderer.setSize(window.innerWidth,height!);
 
     const lineColor = 0x564553;
-    const geometry = new THREE.SphereGeometry( height!/pov, 25, 25 ); 
+    const geometry = new THREE.SphereGeometry( height/pov, 25, 25 ); 
     const basicMaterial = new THREE.MeshBasicMaterial({color: lineColor, wireframe:true});
     const mesh = new THREE.Mesh(geometry, basicMaterial)
     scene.add(mesh)
@@ -37,11 +42,12 @@ const Hero: FC = () => {
       myReq = window.requestAnimationFrame(animate)
     }
     animate();
+    setHeight(document.getElementById("root")!.offsetHeight)
 
     return() => {
       window.cancelAnimationFrame(myReq);
     }
-  },[])
+  },[height])
 
   return (
     <div className="hero">
